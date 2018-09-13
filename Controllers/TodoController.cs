d07885429d985e8b5fe7cd3ede748aa509955b2a
JerryAa <jerrya15@yahoo.com>
2018-09-13 07:47:50 -0500

    Initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Task; 
+using Microsoft.AspNetCore.Mvc; 
+
+namespace AspNetCoreTodo.Controllers
+{ 
+
+	public class TodoController  : Controller 
+	{ 
+
+		// Actions go here 
+
+	} 
+	
+
+} 
